248f6065a2e7669e0c137dbd64dce62e5683dab8
jjykkj <wjdjjykkj@naver.com>
2018-10-28 22:33:24 +0900

    build.gradle 추가

--- app/src/main/java/com/example/heegi/uls_cafesystem/fragments/DefaultFragment.java
+++ app/src/main/java/com/example/heegi/uls_cafesystem/fragments/DefaultFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,15 @@ import com.example.heegi.uls_cafesystem.R;
 
 public class DefaultFragment extends Fragment {
 
+    private Toolbar toolbar;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.default_fragment,container,false);
+
+        rootView.findViewById(R.id.my_toolbar);
+        //툴바 설정
+        setSupportActionBar(toolbar); //툴바를 액션바와 같게 만들어 준다.
 
         return rootView;
     }
