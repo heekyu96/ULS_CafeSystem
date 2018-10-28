3f6b498beba1ccfad4e95507f0a671a2106238a5
HeeGyuYang <imtt96@icloud.com>
2018-10-28 23:05:20 +0900

    Update app/src/main/java/com/example/heegi/uls_cafesystem/fragments/DefaultFragment.java

--- app/src/main/java/com/example/heegi/uls_cafesystem/fragments/DefaultFragment.java
+++ app/src/main/java/com/example/heegi/uls_cafesystem/fragments/DefaultFragment.java
@@ -19,9 +19,6 @@ public class DefaultFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.default_fragment,container,false);
 
-        rootView.findViewById(R.id.my_toolbar);
-        //툴바 설정
-        setSupportActionBar(toolbar); //툴바를 액션바와 같게 만들어 준다.
 
         return rootView;
     }
