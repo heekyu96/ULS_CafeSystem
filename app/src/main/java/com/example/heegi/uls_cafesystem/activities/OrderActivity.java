13836e2869cef183aadde6ec6a447f0a75d39801
HeeGyuYang <imtt96@icloud.com>
2018-10-29 23:57:49 +0900

    repair orderActivity _heegi

--- app/src/main/java/com/example/heegi/uls_cafesystem/activities/OrderActivity.java
+++ app/src/main/java/com/example/heegi/uls_cafesystem/activities/OrderActivity.java
@@ -4,18 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.heegi.uls_cafesystem.R;
 
 public class OrderActivity extends AppCompatActivity {
-    Button kanu;
-    Button maxim;
-    Button nescafe;
-    Button greentea;
+    ImageButton kanu;
+    ImageButton maxim;
+    ImageButton nescafe;
+    ImageButton greentea;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order);
-        kanu = (Button)findViewById(R.id.kanu);
+        kanu = findViewById(R.id.kanu);
+        maxim = findViewById(R.id.maxim);
     }
 }
