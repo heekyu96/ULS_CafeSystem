a86b38fedb29291c7d91be736f73883ce4c282f2
HeeGyuYang <imtt96@icloud.com>
2018-10-29 18:19:56 +0900

    make origin

--- app/src/main/java/com/example/heegi/uls_cafesystem/fragments/DefaultFragment.java
+++ app/src/main/java/com/example/heegi/uls_cafesystem/fragments/DefaultFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +12,6 @@ import com.example.heegi.uls_cafesystem.R;
 
 public class DefaultFragment extends Fragment {
 
-    private Toolbar toolbar;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
