82a659f30c9ecbb6d4bc706cd23058e76971a5ed
tjko96 <tjko9113@gmail.com>
2018-11-01 22:59:30 +0900

    소년점프쉿

--- app/src/main/java/com/example/heegi/uls_cafesystem/fragments/Level3Fragment.java
+++ app/src/main/java/com/example/heegi/uls_cafesystem/fragments/Level3Fragment.java
@@ -9,16 +9,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.heegi.uls_cafesystem.R;
 import com.example.heegi.uls_cafesystem.activities.OrderActivity;
 
 public class Level3Fragment extends Fragment {
 
+    ImageButton light;
+    ImageButton air;
+    ImageButton mc;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.lev3_fragment,container,false);
+        light = rootView.findViewById(R.id.lightbutton);
+        air = rootView.findViewById(R.id.airbutton);
+        mc = rootView.findViewById(R.id.mcbutton);
+
+
 
         return rootView;
     }
